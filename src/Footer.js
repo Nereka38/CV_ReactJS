9a5d482427297d64bd5fa383ac6dc91523e78477
Nereka38 <87275667+Nereka38@users.noreply.github.com>
2023-05-11 12:26:14 +0200

    changes css

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
 
   return (
    <div className="footer">
-   <p>Creado por: <a href="https://github.com/Nereka38">Nereka38</a> / 2023 </p>
+   <p>Creado por: <a href="https://github.com/Nereka38">Nereka38</a> 2023 </p>
    </div>
   );
 }
